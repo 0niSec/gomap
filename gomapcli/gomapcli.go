2ade3d1ab862d7cb549981a0385eefb3bfee6499
Nick <nconkl1@outlook.com>
2024-06-26 23:54:51 -0400

    :sparkles: added default path if ports flag not set

--- gomapcli/gomapcli.go
+++ gomapcli/gomapcli.go
@@ -8,7 +8,20 @@ import (
 )
 
 func Runner(c *cli.Context) error {
-	// Parse the ports from the command line
+	// If the ports flag is not set, scan the top 1000 ports by default
+	// We only need to parse the target in this case ince the ports are already in the proper format
+	if c.String("ports") == "" {
+		target, err := ParseTarget(c.String("target"))
+		if err != nil {
+			return fmt.Errorf("error parsing target: %w", err)
+		}
+
+		ScanPorts(Top1000Ports[:], target, c.Duration("timeout"))
+
+		return nil
+	}
+
+	// If the ports flag is set, scan the ports specified
 	ports, err := ParsePorts(c.String("ports"))
 	if err != nil {
 		return fmt.Errorf("error parsing ports: %w", err)
