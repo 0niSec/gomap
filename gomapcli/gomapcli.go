ce51488ae6354bc05662222fec82d037597b7ca7
Nick <nconkl1@outlook.com>
2024-07-15 14:28:10 -0400

    :recycle: updated Runner

--- gomapcli/gomapcli.go
+++ gomapcli/gomapcli.go
@@ -3,23 +3,37 @@ package gomapcli
 
 import (
 	"fmt"
-	"net"
 
+	"github.com/0niSec/gomap/network"
 	"github.com/0niSec/gomap/scanner"
 	"github.com/urfave/cli/v2"
 )
 
 func Runner(c *cli.Context) error {
-	// If the ports flag is not set, scan the top 1000 ports by default
-	// We only need to parse the target in this case ince the ports are already in the proper format
-	if c.String("ports") == "" {
-		target, err := ParseTarget(c.String("target"))
-		if err != nil {
-			return fmt.Errorf("error parsing target: %w", err)
-		}
 
-		// TODO: Add new Scan function
-		fmt.Printf("Target: %s", target)
+	iface, err := network.GetValidInterface()
+	if err != nil {
+		return fmt.Errorf("error getting valid interface: %w", err)
+	}
+
+	// Get IP Address for interface
+	srcIP, err := network.GetInterfaceIPAddress(iface)
+	if err != nil {
+		return fmt.Errorf("error getting interface IP address: %w", err)
+	}
+
+	target, err := ParseTarget(c.String("target"))
+	if err != nil {
+		return fmt.Errorf("error parsing target: %w", err)
+	}
+
+	// If the ports flag is not set, scan the top 1000 ports by default
+	// We only need to parse the target in this case since the ports are already in the proper format
+	if c.String("ports") == "" {
+		results := scanner.Scan(srcIP, target, Top1000Ports, c.Duration("timeout"))
+		for _, result := range results {
+			fmt.Println(result)
+		}
 
 		return nil
 	}
@@ -30,17 +44,8 @@ func Runner(c *cli.Context) error {
 		return fmt.Errorf("error parsing ports: %w", err)
 	}
 
-	// Parse the target from the command line
-	// target, err := ParseTarget(c.String("target"))
-	// if err != nil {
-	// 	return fmt.Errorf("error parsing target: %w", err)
-	// }
-
-	// ?
-	// TODO: Implement function for obtaining the IP address of the interface
-	results := scanner.Scan(net.ParseIP("192.168.113.57"), net.ParseIP(c.String("target")), ports, c.Duration("timeout"))
+	results := scanner.Scan(srcIP, target, ports, c.Duration("timeout"))
 	for _, result := range results {
-		fmt.Println("Back in gomapcli")
 		fmt.Println(result)
 	}
 
